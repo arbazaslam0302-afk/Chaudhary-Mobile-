0b9a3314df4e19e7b5b235d6f942ea362d5d35b6
arbazaslam0302-afk <arbazaslam0302@gmail.com>
2026-08-08 15:26:39 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(); // Yeh line zaroori hai
   runApp(const ChaudharyMobileApp());
 }
 
